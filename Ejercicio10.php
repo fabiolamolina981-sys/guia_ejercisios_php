c2d9fa00e3d45e4c945f60cf06a360708b69917f
fabiolamolina981-sys <fabiolamolina981@gmail.com>
2026-05-20 09:22:15 -0600

    Update Ejercicio10.php

--- Ejercicio10.php
+++ Ejercicio10.php
@@ -6,6 +6,6 @@ $suma = ($nota1+$nota2+$nota3);
 $promedio =($suma/3);
 echo "Su promedio es:" . $promedio;
 echo "<div style='text-align: right; margin-top: 40px;'>";
-echo "<a href='index.php'>Volver al Menú Principal</a>";
+echo "<a href='index.html'>Volver al Menú Principal</a>";
 echo "</div>";
 ?>
