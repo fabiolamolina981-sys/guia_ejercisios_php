49d720bd039fc7820eb69e836e39be82cbf4a2cd
fabiolamolina981-sys <fabiolamolina981@gmail.com>
2026-05-20 09:32:05 -0600

    Update Ejercicio6.php

--- Ejercicio6.php
+++ Ejercicio6.php
@@ -6,6 +6,6 @@ echo "<center><h1>Su nombre es:" . $nombre;
 echo "<center>Su edad es:" . $edad;
 echo "</h1>";
 echo "<div style='text-align: right; margin-top: 40px;'>";
-echo "<a href='index.php'>Volver al Menú Principal</a>";
+echo "<a href='index.html'>Volver al Menú Principal</a>";
 echo "</div>";
 ?>
