5aafd2d88e325e7f7301cea0140154dbad855899
fabiolamolina981-sys <fabiolamolina981@gmail.com>
2026-05-20 09:29:17 -0600

    Update Ejercicio25.php

--- Ejercicio25.php
+++ Ejercicio25.php
@@ -5,6 +5,6 @@ $ciudad = $_POST ['ciudad'];
 
 echo "Hola, " . $nombre . " con correo: " . $correo . " y vive en la ciudad de " . $ciudad;
 echo "<div style='text-align: right; margin-top: 40px;'>";
-echo "<a href='index.php'>Volver al Menú Principal</a>";
+echo "<a href='index.html'>Volver al Menú Principal</a>";
 echo "</div>";
 ?>
