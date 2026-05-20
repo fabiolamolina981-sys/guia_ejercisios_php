96d0411d1cf5dd809d53543d6382d629369da4d9
fabiolamolina981-sys <fabiolamolina981@gmail.com>
2026-05-20 09:29:04 -0600

    Update Ejercicio24.php

--- Ejercicio24.php
+++ Ejercicio24.php
@@ -3,6 +3,6 @@ $edad = $_POST ['edad'];
 $futura = ($edad + 10);
 echo "Su edad en 10 años sera :" . $futura;
 echo "<div style='text-align: right; margin-top: 40px;'>";
-echo "<a href='index.php'>Volver al Menú Principal</a>";
+echo "<a href='index.html'>Volver al Menú Principal</a>";
 echo "</div>";
 ?>
