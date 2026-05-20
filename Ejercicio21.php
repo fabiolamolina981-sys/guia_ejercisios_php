0559bc9153c22560fbba6cc62ff5077d21a9c6e5
fabiolamolina981-sys <fabiolamolina981@gmail.com>
2026-05-20 09:26:29 -0600

    Update Ejercicio21.php

--- Ejercicio21.php
+++ Ejercicio21.php
@@ -3,6 +3,6 @@ $palabra = $_POST['palabra'];
 $cantidad_letras = strlen($palabra);
 echo "La palabra '" . $palabra . "' tiene " . $cantidad_letras . " letras.";
 echo "<div style='text-align: right; margin-top: 40px;'>";
-echo "<a href='index.php'>Volver al Menú Principal</a>";
+echo "<a href='index.html'>Volver al Menú Principal</a>";
 echo "</div>";
 ?>
