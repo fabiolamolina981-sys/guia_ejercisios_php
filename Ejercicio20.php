787fe7b16291077a527e52810195e4926b6b2abd
fabiolamolina981-sys <fabiolamolina981@gmail.com>
2026-05-20 09:26:04 -0600

    Update Ejercicio20.php

--- Ejercicio20.php
+++ Ejercicio20.php
@@ -6,6 +6,6 @@ for ($i = 1; $i <= $num; $i++) {
 } 
 echo "La suma de los números desde 1 hasta " . $num . " es: " . $suma;
 echo "<div style='text-align: right; margin-top: 40px;'>";
-echo "<a href='index.php'>Volver al Menú Principal</a>";
+echo "<a href='index.html'>Volver al Menú Principal</a>";
 echo "</div>";
 ?>
