5563da6d2b819e3b4b3de99be22408248f86540c
fabiolamolina981-sys <fabiolamolina981@gmail.com>
2026-05-20 09:25:12 -0600

    Update Ejercicio19.php

--- Ejercicio19.php
+++ Ejercicio19.php
@@ -8,6 +8,6 @@ for ($i = 1; $i <= 10; $i++) {
     echo $tabla . " x " . $i . " = " . $resultado . "<br>";  
 }
 echo "<div style='text-align: right; margin-top: 40px;'>";
-echo "<a href='index.php'>Volver al Menú Principal</a>";
+echo "<a href='index.html'>Volver al Menú Principal</a>";
 echo "</div>";
 ?>
