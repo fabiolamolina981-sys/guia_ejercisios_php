9fbeaa853127a33261fac0a3505629dcad569c06
fabiolamolina981-sys <fabiolamolina981@gmail.com>
2026-05-20 09:32:51 -0600

    Update Ejercicio9.php

--- Ejercicio9.php
+++ Ejercicio9.php
@@ -10,6 +10,6 @@ else {
 }
 echo "</h1>";
 echo "<div style='text-align: right; margin-top: 40px;'>";
-echo "<a href='index.php'>Volver al Menú Principal</a>";
+echo "<a href='index.html'>Volver al Menú Principal</a>";
 echo "</div>";
 ?>
