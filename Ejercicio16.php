51cb7fda5f80375836ae47c97c3f7afe5ea39438
fabiolamolina981-sys <fabiolamolina981@gmail.com>
2026-05-20 09:24:04 -0600

    Update Ejercicio16.php

--- Ejercicio16.php
+++ Ejercicio16.php
@@ -5,6 +5,6 @@ $pago = $_POST ['pago'];
 $sueldo = ($horas * $pago);
 echo "El sueldo de: " . $nombre . " es de: $" . $sueldo;
 echo "<div style='text-align: right; margin-top: 40px;'>";
-echo "<a href='index.php'>Volver al Menú Principal</a>";
+echo "<a href='index.html'>Volver al Menú Principal</a>";
 echo "</div>";
 ?>
